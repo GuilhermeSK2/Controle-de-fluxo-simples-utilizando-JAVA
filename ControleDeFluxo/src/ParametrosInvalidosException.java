88519c58441aa1f20dcf47807800c088eb2e9cda
Guilherme Freitas (Software Developer) <139295562+GuilhermeSK2@users.noreply.github.com>
2024-10-10 21:34:10 -0300

    Add files via upload

---
+++ ControleDeFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,5 @@
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message); // Chama o construtor da classe Exception para inicializar a mensagem de erro
+    }
+}
